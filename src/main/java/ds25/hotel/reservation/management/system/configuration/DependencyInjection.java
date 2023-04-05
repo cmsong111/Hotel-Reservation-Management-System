1a64b3459a30f1f4b4235cbb1da9030d096fc435
Namju Kim <cmsong111@naver.com>
2023-04-06 03:24:58 +0900

    Update DependencyInjection.java

--- src/main/java/ds25/hotel/reservation/management/system/configuration/DependencyInjection.java
+++ src/main/java/ds25/hotel/reservation/management/system/configuration/DependencyInjection.java
@@ -32,10 +32,12 @@ public class DependencyInjection {
         properties.put("jakarta.persistence.jdbc.password", "");
         properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
 
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.use_sql_comments", "true");
         properties.put("hibernate.id.new_generator_mappings", "true");
+
+        properties.put("hibernate.hbm2ddl.auto", "create");
 
 
         PersistenceProvider provider = new HibernatePersistenceProvider();
