cbf9ee1d2c841bb6dc0dff7bb87eff8d9eef5102
Namju Kim <cmsong111@naver.com>
2023-04-07 13:09:41 +0900

    Revert "feat: Hotel Review Service, Repositroy 등록"
    This reverts commit 32de0432d0be4ee6fb539e4a8e63beffa85b80a1.

--- src/main/java/ds25/hotel/reservation/management/system/entity/hotel/HotelReview.java
+++ src/main/java/ds25/hotel/reservation/management/system/entity/hotel/HotelReview.java
@@ -1,17 +1,9 @@
 package ds25.hotel.reservation.management.system.entity.hotel;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.Date;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 public class HotelReview  {
     int idx;
     int hotelIdx;
--- src/main/java/ds25/hotel/reservation/management/system/service/hotel/HotelReviewService.java
+++
@@ -1,9 +0,0 @@
-package ds25.hotel.reservation.management.system.service.hotel;
-
-import ds25.hotel.reservation.management.system.configuration.Singleton;
-import ds25.hotel.reservation.management.system.repository.hotel.HotelReviewRepository;
-
-public class HotelReviewService {
-    HotelReviewRepository hotelReviewRepository = Singleton.getInstance().getHotelReviewRepository();
-
-}
