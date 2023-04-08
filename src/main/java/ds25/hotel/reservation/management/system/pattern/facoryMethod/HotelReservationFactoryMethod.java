39a0dbdbb2d304019b6376760553eb1953e5b6ad
Namju Kim <cmsong111@naver.com>
2023-04-08 21:57:53 +0900

    feat: 팩토리 메소드 추가

--- src/main/java/ds25/hotel/reservation/management/system/pattern/facoryMethod/HotelReservationFactoryMethod.java
+++ src/main/java/ds25/hotel/reservation/management/system/pattern/facoryMethod/HotelReservationFactoryMethod.java
@@ -1,10 +1,18 @@
 package ds25.hotel.reservation.management.system.pattern.facoryMethod;
 
-import ds25.hotel.reservation.management.system.entity.hotel.HotelReservation;
+import ds25.hotel.reservation.management.system.configuration.Singleton;
+import ds25.hotel.reservation.management.system.dto.hotel.HotelReservationDto;
 
 public class HotelReservationFactoryMethod {
 
+    public static HotelReservationDto createReservation(Long HotelRoomIdx) {
 
+        return HotelReservationDto.builder()
+                .hotelIdx(HotelRoomIdx)
+                .userId(Singleton.getInstance().getUserProvider().getUser().get().getId())
+                .peopleCount(2)
+                .hotelRoomIdx(HotelRoomIdx)
+                .build();
 
-
+    }
 }
