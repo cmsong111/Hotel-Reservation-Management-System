6bb9e9ee6b90bbf381f23b7029d0e3bf996cde13
Namju Kim <cmsong111@naver.com>
2023-04-25 04:58:58 +0900

    feat: data.sql 방법 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- User 데이터 입력
+insert into users (id, password, name, phone, email, role, grade)
+values ('userId', 'userPassword', 'userName', '010-1234-5678', 'user@example.com', 1, 1);
+
+-- Image 데이터 입력
+
+-- Hotel 데이터 입력
+
+-- Hotel Room Type 데이터 입력
+
+-- Hotel Room 데이터 입력
+
+-- Hotel 리뷰 데이터 입력
