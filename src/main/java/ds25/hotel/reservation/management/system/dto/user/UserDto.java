5358055c0c613384ec961b60d79afedf6ed4a8bc
Namju Kim <cmsong111@naver.com>
2023-04-30 15:27:39 +0900

    refactor: UserDto Builder 패턴 적용

--- src/main/java/ds25/hotel/reservation/management/system/dto/user/UserDto.java
+++ src/main/java/ds25/hotel/reservation/management/system/dto/user/UserDto.java
@@ -19,4 +19,68 @@ public class UserDto {
     String email;
     UserRole role;
     UserGrade grade;
+
+    public static class Builder {
+        private String id;
+        private String password;
+        private String name;
+        private String phone;
+        private String email;
+        private UserRole role;
+        private UserGrade grade;
+
+        public static builder builder() {
+            return new builder();
+        }
+
+        public static class builder {
+            private String id;
+            private String password;
+            private String name;
+            private String phone;
+            private String email;
+            private UserRole role;
+            private UserGrade grade;
+
+            public builder id(String id) {
+                this.id = id;
+                return this;
+            }
+
+            public builder password(String password) {
+                this.password = password;
+                return this;
+            }
+
+            public builder name(String name) {
+                this.name = name;
+                return this;
+            }
+
+            public builder phone(String phone) {
+                this.phone = phone;
+                return this;
+            }
+
+            public builder email(String email) {
+                this.email = email;
+                return this;
+            }
+
+            public builder role(UserRole role) {
+                this.role = role;
+                return this;
+            }
+
+            public builder grade(UserGrade grade) {
+                this.grade = grade;
+                return this;
+            }
+
+            public UserDto build() {
+                // perform any desired validation checks here
+                return new UserDto(id, password, name, phone, email, role, grade);
+            }
+        }
+    }
 }
